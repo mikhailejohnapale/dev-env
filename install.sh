7c7c19e8e4696650623fbfa25ac2451f0f4a5116
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-03-24 13:12:27 +0800

    replaced sublime with atom

--- install.sh
+++ install.sh
@@ -64,17 +64,15 @@ sudo apt-get install nautilus-dropbox
 
 
 
-# sublime text 3 install
-sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-sudo apt-get update
-sudo apt-get install sublime-text-installer
+# atom
+cd ~/Downloads
+# go to https://atom.io/
+# download latest release
+sudo dpkg -i atom-amd64.deb
 
-# install package manager in sublime text 3
-# `Ctrl ~` and paste code below and wait
-import urllib.request,os,hashlib; h = 'eb2297e1a458f27d836c04bb0cbaf282' + 'd0e7a3098092775ccb37ca9d6b2e4b7d'; pf = 'Package Control.sublime-package'; ipp = sublime.installed_packages_path(); urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) ); by = urllib.request.urlopen( 'http://packagecontrol.io/' + pf.replace(' ', '%20')).read(); dh = hashlib.sha256(by).hexdigest(); print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by)
-
-# visite the link below to sync with dropbox
-# https://packagecontrol.io/docs/syncing#dropbox-linux
+# atom packages
+apm install atom-terminal
+apm install set-syntax
 
 
 
