47e5e4e0d4542907036c745649770b30594c8b83
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-03-25 11:24:25 +0800

    revised

--- install.sh
+++ install.sh
@@ -4,34 +4,32 @@
 
 # update and upgrade
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 
 
 
-# select, download and install programming font of choice
+# visit, select, download and install programming font of choice
 # http://programmingfonts.org/
 
 
 
-# install git
-sudo apt-get install git
+# git
+sudo apt-get install -y git
 
 # git configuration
-git config --global \
-	user.name "Jan Maghuyop" \
-	user.email jangmaghuyop@gmail.com \
-	color.ui auto \
-	color.branch auto \
-	color.diff auto \
-	color.status.auto
-	credential.helper cache
+git config --global user.name "Jan Maghuyop"
+git config --global user.email jangmaghuyop@gmail.com
+git config --global color.ui auto
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.status.auto
 
 
 
 # ssh config
 ssh-keygen -t rsa -C "jangmaghuyop@gmail.com"
 
-# register generated keys to github
+# register generated keys to github, bitbucket, et. al.
 # https://help.github.com/articles/generating-ssh-keys/
 
 
@@ -39,17 +37,18 @@ ssh-keygen -t rsa -C "jangmaghuyop@gmail.com"
 # teamviewer
 wget http://download.teamviewer.com/download/teamviewer_linux.deb
 sudo dpkg -i teamviewer_linux.deb
+# install dependencies
 sudo apt-get install -f
-sudo teamviewer --daemon disable
+sudo dpkg -i teamviewer_linux.deb
 
 
 
 # virtualbox
-sudo apt-get install dkms
+sudo apt-get install -y dkms
 sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian trusty contrib" >> /etc/apt/sources.list.d/virtualbox.list'
 wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install virtualbox-4.3
+sudo apt-get install -y virtualbox-4.3
 
 
 
@@ -59,15 +58,15 @@ sudo dpkg -i vagrant_1.7.2_x86_64.deb
 
 
 
-# install dropbox
-sudo apt-get install nautilus-dropbox
+# dropbox
+sudo apt-get install -y nautilus-dropbox
 
 
 
 # atom
 cd ~/Downloads
 # go to https://atom.io/
-# download latest release
+# download latest deb release
 sudo dpkg -i atom-amd64.deb
 
 # atom packages
@@ -77,10 +76,8 @@ apm install autocomplete-plus
 
 
 
-# install terminator
-sudo apt-get install terminator
-
-
+# terminator
+sudo apt-get install -y terminator
 
 # copy paste terminator
 subl ~/.config/terminator/config
@@ -90,31 +87,30 @@ subl ~/.config/terminator/config
 
 
 
-# install hipchat
+# hipchat
 sudo su
-echo "deb http://downloads.hipchat.com/linux/apt stable main" > \
-	  /etc/apt/sources.list.d/atlassian-hipchat.list
+echo "deb http://downloads.hipchat.com/linux/apt stable main" > /etc/apt/sources.list.d/atlassian-hipchat.list
 wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add -
 apt-get update
-apt-get install hipchat
+apt-get install -y hipchat
 
 
 
-# install postgres db server
+# postgres
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 sudo apt-get install wget ca-certificates
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install postgresql-9.4
+sudo apt-get upgrade -y
+sudo apt-get install -y postgresql-9.4
 
 # open postgresql
 sudo -u postgres psql postgres
 
 
 
-# install nodejs
-sudo apt-get install build-essential libssl-dev curl
+# nodejs
+sudo apt-get install -y build-essential libssl-dev curl
 curl https://raw.githubusercontent.com/creationix/nvm/v0.20.0/install.sh | sh
 echo "source ~/.nvm/nvm.sh" >> ~/.bashrc
 # restart your terminal
@@ -124,19 +120,22 @@ nvm alias default v0.12.0
 
 
 
-# install bower
+# bower
 npm install -g bower
 
 
 
-# install z
+# z
 cd ~
 git clone git@github.com:rupa/z.git .bin/z/
+echo '. /home/$(whoami)/.bin/z/z.sh' >> ~./bashrc
 
 
 
-# install flash
-# download latest flash plugin in adobe website in tar.gz format
+# flash plugin for firefox
+cd ~/Downloads
+# go to https://get.adobe.com/flashplayer/
+# download tar.gz format
 gunzip install_flash_player_11_linux.x86_64.tar.gz
 tar -xvf install_flash_player_11_linux.x86_64.tar
 sudo cp libflashplayer.so /usr/lib/firefox/browser/plugins
@@ -144,16 +143,16 @@ sudo cp libflashplayer.so /usr/lib/firefox/browser/plugins
 
 
 # python dependencies
-sudo apt-get install python3-dev
+sudo apt-get install -y python3-dev
 
 
 
-# install pip3
+# pip3
 cd ~/Downloads
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 
 
 
-# install virtualenv in user
+# python virtualenv in user
 pip3 install --user virtualenv
